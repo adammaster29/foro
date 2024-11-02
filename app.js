d89eb400b209b791686ac371c88bfc488881c999
ADAM <adammaster29@gmail.com>
2024-11-01 21:12:42 -0500

    file/add-cors

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authRoutes = require('./routes/auth');
 const temaRoutes = require('./routes/temas');
+const cors = require('cors')
 const publicacionesRoutes = require('./routes/publicaciones');
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/temas', temaRoutes);
 app.use('/api/publicaciones', publicacionesRoutes);
